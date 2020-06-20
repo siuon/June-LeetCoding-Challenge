5008b90a05e4eafe5cd77c74ea6b880e0f5ca7ed
Siuon Tom <sotom@sotom.tw>
2020-06-20 20:21:10 +0800

    add day 19, Longest Duplicate Substring, not complete yet :(

---
+++ 019_Longest_Duplicate_Substring/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _019_Longest_Duplicate_Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Given a string S, consider all duplicated substrings: (contiguous) substrings of S that occur 2 or more times.  
+               (The occurrences may overlap.)
+   
+               Return any duplicated substring that has the longest possible length.  (If S does not have a duplicated substring, 
+               the answer is "".)
+   
+               Example 1:
+               Input: "banana"
+               Output: "ana"
+
+               Note:
+                 2 <= S.length <= 10^5
+                 S consists of lowercase English letters.
+            */
+
+            var S = "banana";
+
+            var solution = new Solution();
+
+            Console.WriteLine(solution.LongestDupSubstring(S) == "ana");
+        }
+    }
+
+    public class Solution
+    {
+        public string LongestDupSubstring(string S)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
