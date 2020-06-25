419d577cff001f1b9ca735b219aad7111cd12ccc
siuon <sotom@sotom.tw>
2020-06-25 22:41:05 +0800

    add day 24, Unique Binary Search Trees, not complete yet :(

---
+++ 024_Unique_Binary_Search_Trees/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _024_Unique_Binary_Search_Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+   
+               Example:
+               Input: 3
+               Output: 5
+
+               Explanation:
+               Given n = 3, there are a total of 5 unique BST's:
+   
+                  1         3     3      2      1
+                   \       /     /      / \      \
+                    3     2     1      1   3      2
+                   /     /       \                 \
+                  2     1         2                 3
+            */
+
+            var n = 3;
+
+            var solution = new Solution();
+
+            Console.WriteLine(solution.NumTrees(n) == 5);
+        }
+    }
+
+    public class Solution
+    {
+        public int NumTrees(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
