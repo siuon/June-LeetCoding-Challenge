c64bce31b8835d0585a623eebe7697dbfd282a0b
siuon <sotom@sotom.tw>
2020-06-28 16:32:57 +0800

    add day 27, Perfect Squares, not complete yet :(

---
+++ 027_Perfect_Squares/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _027_Perfect_Squares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+   
+               Example 1:
+   
+               Input: n = 12
+               Output: 3 
+               Explanation: 12 = 4 + 4 + 4.
+            */
+
+            var n = 12;
+
+            var solution = new Solution();
+
+            Console.WriteLine(solution.NumSquares(n) == 3);
+        }
+    }
+
+    public class Solution
+    {
+        public int NumSquares(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
